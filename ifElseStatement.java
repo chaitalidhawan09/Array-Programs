cb52d9b0a2e17f60e1251a4944caa6a55e3f5b02
chaitalidhawan09 <chaitalidhawan09@gmail.com>
2024-02-01 13:18:01 +0530

    Conditional Statements Programs

---
+++ ifElseStatement.java
@@ -0,0 +1,27 @@
+package conditionalStatements;
+
+public class ifElseStatement 
+{
+
+	public ifElseStatement() 
+	{
+
+	
+	}
+
+	public static void main(String[] args) 
+	{
+
+		int number = -5;
+
+		if (number > 0) 
+		{
+		    System.out.println("The number is positive.");
+		} 
+		else 
+		{
+		    System.out.println("The number is not positive.");
+		}
+	}
+
+}
---
+++ ifStatement.java
@@ -0,0 +1,23 @@
+package conditionalStatements;
+
+public class ifStatement 
+{
+
+	public ifStatement() 
+	{
+
+	
+	}
+
+	public static void main(String[] args)
+	{
+
+		int number = 10;
+
+		if (number > 0)
+		{
+		    System.out.println("The number is positive.");
+		}
+	}
+
+}
